d102c2a6243ab40699139d1085244dfbb3ddadd2
mwong01 <mattw90@gmail.com>
2019-11-05 20:46:10 +0000

    create js file to Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  console.log("ready!");
+});
+
+// const textarea = function (text) {
+//   $("new-tweet > form > textbox")
+// }
+
+// $(function() {
+
+// }
+// )  
