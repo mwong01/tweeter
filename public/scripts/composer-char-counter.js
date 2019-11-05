bf5cabcb9b0ab6083e6489c62e120dc88d1312f4
mwong01 <mattw90@gmail.com>
2019-11-05 21:58:25 +0000

    add character count function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-  console.log("ready!");
+  $(".new-tweet").on("keydown keyup","textarea", function() {
+    
+    // create stored variable to determine # chars left
+    let $charInTweet = $(this).val();
+    let $charRemain = 140 - $charInTweet.length;
+
+    // traverse up and down the DOM to get character count
+    let $counter = $(this).closest("form").find(".counter");
+    $counter.text($charRemain);
+
+    // make counter red when tweet > 140 chars
+    if ($charRemain < 0) {
+      $counter.addClass("overtweeted");
+    }
+  });
 });
-
-// const textarea = function (text) {
-//   $("new-tweet > form > textbox")
-// }
-
-// $(function() {
-
-// }
-// )  
